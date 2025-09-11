018b232598c8b621790e28eddc635dc50cb94814
mourha <114663632+mourha@users.noreply.github.com>
2025-09-11 15:55:36 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+#Day 1 — Nick Name Generator
+
+# ✅ PROJECT: Nick Name Generator
+# 1) Ask the user for the city they grew up in -> input()
+# 2) Ask the user for the name of a pet -> input()
+# 3) Strip extra spaces and title-case both strings -> .strip().title()
+# 4) Concatenate city + space + pet into a single Nick name string
+# 5) Print a friendly message with the generated name -> print(f"Your Nick name could be {name}")
+# 6) Add emojis or ASCII art to make it fun
+
+# (Optional)
+# 7) Handle empty inputs: if either is empty, prompt again until valid
+# 8) Allow the user to generate multiple names without restarting (loop + 'y/n')
